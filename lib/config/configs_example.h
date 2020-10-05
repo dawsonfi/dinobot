1ec0f86fb04d12ed5e7115d093e397732a3a11ce
Dawson Israel <dawfre@amazon.com>
2020-10-05 14:58:19 -0300

    Add code

---
+++ lib/config/configs_example.h
@@ -0,0 +1,9 @@
+#define NETWORK_SSID ""
+#define NETWORK_PASSWORD ""
+
+#define MQTT_SERVER "broker.emqx.io"
+#define MQTT_SERVER_PORT 1883
+#define MQTT_USERNAME NULL
+#define MQTT_PASSWORD NULL
+#define MQTT_TOPIC ""
+#define TARGET ""
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include "network/wifi.h"
+#include "mqtt/mqtt_client.h"
+#include "node_mapping/node_mapping.h"
+#include <configs.h>
+
+#define DINOBOT_ON_SWITCH D5
+
+void process_machine_state(char* topic, byte* payload, unsigned int length);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(DINOBOT_ON_SWITCH, OUTPUT);
+
+  setupMQTT((char *) MQTT_SERVER,
+            MQTT_SERVER_PORT,
+            (char *) MQTT_USERNAME,
+            (char *) MQTT_PASSWORD,
+            false,
+            process_machine_state);
+
+  setupWifi((char *) NETWORK_SSID, (char *) NETWORK_PASSWORD, (char *) TARGET);
+  subscribe((char *) MQTT_TOPIC);
+}
+
+void loop() {
+  connectWifi((char *) NETWORK_SSID, (char *) NETWORK_PASSWORD, (char *) TARGET);
+  loopUpdateServer();
+  loopMQTT();
+  delay(200);
+}
+
+void process_machine_state(char* topic, byte* payload, unsigned int length) {
+  String payloadStr = String((char *) payload).substring(0, length);
+  if (payloadStr.indexOf("dinobot=on") >= 0) {
+    Serial.println("Turning ON");
+    digitalWrite(DINOBOT_ON_SWITCH, HIGH);
+  } else if (payloadStr.indexOf("dinobot=off") >= 0) {
+    Serial.println("Turning Off");
+    digitalWrite(DINOBOT_ON_SWITCH, LOW);
+  }
+}
